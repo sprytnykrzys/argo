8010e63e46721e16e1014f88da9a0eaa4f200a4b
sprytnykrzys <krzysztoforganek@onet.pl>
2017-02-11 19:34:38 +0100

    zapis wyboru jezyka

--- controllers/MainCtrl.js
+++ controllers/MainCtrl.js
@@ -10,84 +10,82 @@ angular
         '$localStorage',
         '$rootScope',
         function($scope, $state, $timeout, $localStorage, $rootScope) {
-         $(".button-collapse").sideNav();
+            $(".button-collapse").sideNav();
 
-         $scope.mainMenuItems = [
-         {
-            'labelPl': 'Strona główna',
-            'labelEn': 'Home',
-            'labelRu': 'дома',
-            'activeStateRule': 'main',
-            'uiSref': 'main'
-         },
-         {
-            'labelPl': 'Produkty',
-            'labelEn': 'Products',
-            'labelRu': 'продукты',
-            'activeStateRule': 'mainProducts',
-            'uiSref': 'mainProducts'
-         },
-         {
-            'labelPl': 'O nas',
-            'labelEn': 'About us',
-            'labelRu': 'О компании',
-            'activeStateRule': 'history',
-            'uiSref': 'history'
-         },
-         {
-            'labelPl': 'Kontakt',
-            'labelEn': 'Contact',
-            'labelRu': 'контакт',
-            'activeStateRule': 'contact',
-            'uiSref': 'contact'
-         }
-         ];
+            $scope.mainMenuItems = [{
+                'labelPl': 'Strona główna',
+                'labelEn': 'Home',
+                'labelRu': 'дома',
+                'activeStateRule': 'main',
+                'uiSref': 'main'
+            }, {
+                'labelPl': 'Produkty',
+                'labelEn': 'Products',
+                'labelRu': 'продукты',
+                'activeStateRule': 'mainProducts',
+                'uiSref': 'mainProducts'
+            }, {
+                'labelPl': 'O nas',
+                'labelEn': 'About us',
+                'labelRu': 'О компании',
+                'activeStateRule': 'history',
+                'uiSref': 'history'
+            }, {
+                'labelPl': 'Kontakt',
+                'labelEn': 'Contact',
+                'labelRu': 'контакт',
+                'activeStateRule': 'contact',
+                'uiSref': 'contact'
+            }];
 
-         $scope.globalLang = [
-         {
-            'langCode': 'pl',
-            'flag': 'img/pl.png',
-            'selected': true
-         },
-         {
-            'langCode': 'en',
-            'flag': 'img/en.png',
-            'selected': false
-         },
-         {
-            'langCode': 'ru',
-            'flag': 'img/ru.png',
-            'selected': false
-         }
-         ];
+            $scope.globalLang = [{
+                'langCode': 'pl',
+                'flag': 'img/pl.png',
+            }, {
+                'langCode': 'en',
+                'flag': 'img/en.png',
+            }, {
+                'langCode': 'ru',
+                'flag': 'img/ru.png',
+            }];
 
 
-         $scope.changeState = function(state){
-                $state.go(state);  
-         };
+            $scope.changeState = function(state) {
+                $state.go(state);
+            };
 
 
-        $scope.selectLang = function(lang){
-            $rootScope.lang = lang;
-            $localStorage.currLang = lang;
-            for(var i=0; i<$scope.globalLang.length; i++){
-                /*if($scope.globalLang[i].selected == true){
-                    $scope.globalLang[i].selected = false;  
-                }else{
-                    $scope.globalLang[i].selected = true;
+            $scope.selectLang = function(lang) {
+                $rootScope.lang = lang;
+                $localStorage.currLang = lang;
+                for (var i = 0; i < $scope.globalLang.length; i++) {
+                    if ($scope.globalLang[i].langCode == lang) {
+                        $scope.globalLang[i].selected = true;
+                    } else {
+                        $scope.globalLang[i].selected = false;
+                    }
+                }
+                $('#mainlang').removeClass('active')
+            };
 
-                }*/
-                $scope.globalLang[i].selected = false;
-            }
-            if(lang == 'pl'){
-                $scope.globalLang[0].selected = true;
-            }else if(lang == 'en'){
-                $scope.globalLang[1].selected = true;
-            }else{
-                $scope.globalLang[2].selected = true;
-            }
-            $('#mainlang').removeClass('active')
-         };
-        
+            $scope.init = function() {
+                if ($localStorage.currLang) {
+                    for (var i = 0; i < $scope.globalLang.length; i++) {
+                        if ($scope.globalLang[i].langCode == $localStorage.currLang) {
+                            $scope.globalLang[i].selected = true;
+                        }
+                    }
+                } else {
+                    for (var i = 0; i < $scope.globalLang.length; i++) {
+                        if ($scope.globalLang[i].langCode == 'pl') {
+                            $scope.globalLang[i].selected = true;
+                        }
+                    }
+                }
+            };
+
+            $scope.init();
+
+
         }
     ]);
